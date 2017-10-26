67c608f4c070769f419b7ad8ba9883fdbf9213f3
apedano <pedano.alessandro@gmail.com>
2017-10-26 16:45:27 +0200

    Modified server.js

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const app = express();
 
+// dist folder is generated after ng build is run on the heroku server
 app.use(express.static(__dirname, '/dist'));
+//set the port to listen from outside
 app.listen(process.env.port || 8080);
